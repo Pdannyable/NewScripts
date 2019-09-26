e6c32b9ae6616c7e7f0c60e120604080ab0f063f
Jason Eckert <jason.eckert@trios.com>
2019-09-26 11:53:33 -0400

    My first commit

---
+++ script1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This script creates a report of our disk configuration
+
+FILENAME=`hostname`
+echo "Disk report saved to $FILENAME.report"
+
+echo -e "\n LVM Configuration: \n\n" >>$FILENAME.report
+lvscan >>$FILENAME.report
+vgscan >>$FILENAME.report
+pvscan >>$FILENAME.report
+
+echo -e "\n\n Partition Configuration: \n\n" >>$FILENAME.report
+lsblk -f >>$FILENAME.report
+
+echo -e "\n\n Mounted Filesystem Usage: \n\n" >>$FILENAME.report
+df -h | grep -v tmp >>$FILENAME.report
