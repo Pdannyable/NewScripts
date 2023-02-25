a063143c7238af32703c55a492df28d23117cf7d
Jadon Oyinloye <danoyinloye@outlook.com>
2023-02-25 18:53:29 -0500

    List of team members on Paypal project

---
+++ teammembers.java
@@ -0,0 +1,14 @@
+# This is a list of Team members working on Paypal project
+# Team lead is yet to be assigned
+# On the interim, Ali is designated to lead.
+Saturn
+Jupiter
+Ali
+Pluto
+Mandalorian
+Jagunlabi
+Orion
+Venus
+Mars
+Uranus
+Earth
