6729d4c94a0726e56a91378cdfe22bc300faa6b9
Pdannyable <125929649+Pdannyable@users.noreply.github.com>
2023-03-01 23:21:14 -0600

    Staging completedReady to merge

---
+++ arena.py
@@ -0,0 +1,6 @@
+#The arena compilation is made of nothing more than oven crater
+#created soley for the refreshement of whoever drinks it.
+echo "This who dwell in the secret place of the Most High will abide under the shadow of the Almighty."
+echo "we will say of the Lord,"
+echo "He is our protection and amour"
+#End of script
---
+++ clubber.java
@@ -0,0 +1,11 @@
+#this is another app created for the clubber identity forum
+#the names included are from the regular meeting attendants
+1. Scrabble
+2. Chess
+3. Draught
+4. Chinese Checkers
+5. Dominos
+6. Monopoly
+7. Poker
+8. Cards
+Pack of cards
